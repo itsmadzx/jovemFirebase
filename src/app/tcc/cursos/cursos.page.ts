baf54b4b767c330cb1ac8cf44d2d425b0352705f
itsmadzx <eduardamaria301208@gmail.com>
2026-09-22 03:33:45 -0300

    html, css e ts da página de cursos

--- src/app/tcc/cursos/cursos.page.ts
+++ src/app/tcc/cursos/cursos.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Instituicao {
+  nome: string;
+  descricao: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.page.html',
@@ -8,9 +14,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosPage implements OnInit {
 
-  constructor() { }
+  instituicoes: Instituicao[] = [
+
+    {
+      nome: 'Fundação Bradesco',
+      descricao: 'Cursos gratuitos online com certificado em áreas como administração, tecnologia e desenvolvimento profissional.',
+      link: 'https://www.ev.org.br/cursos'
+    },
+
+    {
+      nome: 'SENAI',
+      descricao: 'Cursos gratuitos e profissionalizantes para aprender novas habilidades e se preparar para o mercado de trabalho.',
+      link: 'https://www.sp.senai.br/noticia/conheca-os-cursos-livres-gratuitos-do-senai-sp'
+    },
+
+    {
+      nome: 'Sebrae',
+      descricao: 'Cursos sobre empreendedorismo, carreira, marketing, finanças e desenvolvimento profissional.',
+      link: 'https://loja.sebrae.com.br/cursos/cursos-online'
+    },
+
+    {
+      nome: 'Senac',
+      descricao: 'Cursos livres, técnicos e outras oportunidades de formação em diferentes áreas profissionais.',
+      link: 'https://www.ead.senac.br/cursos-por-area/'
+    }
+
+  ];
+
+  instituicoesFiltradas: Instituicao[] = [];
+
+  pesquisa = '';
+
+  constructor() {}
 
   ngOnInit() {
+    this.instituicoesFiltradas = [...this.instituicoes];
+  }
+
+  pesquisar() {
+
+    const texto = this.pesquisa.toLowerCase().trim();
+
+    this.instituicoesFiltradas = this.instituicoes.filter(instituicao =>
+      instituicao.nome.toLowerCase().includes(texto) ||
+      instituicao.descricao.toLowerCase().includes(texto)
+    );
+
   }
 
 }
